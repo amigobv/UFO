66967053eb3de14059eab9f742f8c08a429ba083
amigobv <daniell.rotaru@gmail.com>
2015-10-14 21:44:06 +0200

    SQL scripts created

---
+++ Database/UfoDropTables.sql
@@ -0,0 +1,7 @@
+drop table `UFO_DB`.`Schedule`;
+drop table `UFO_DB`.`Performance`;
+drop table `UFO_DB`.`Venue`;
+drop table `UFO_DB`.`Artist`;
+drop table `UFO_DB`.`Category`;
+drop table `UFO_DB`.`User` ;
+drop schema `UFO_DB`;
