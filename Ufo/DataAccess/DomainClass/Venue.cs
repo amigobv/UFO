20a78b953cbeaa062dc985b4004878d85f38b033
amigobv <daniell.rotaru@gmail.com>
2015-10-16 20:48:12 +0200

    Domain Model and DAO

---
+++ Ufo/DataAccess/DomainClass/Venue.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DomainClass
+{
+    class Venue : IDomain
+    {
+        #region properties
+        public int Id { get; set; }
+        public string Label { get; set; }
+        public string Identifier { get; set; }
+        public string Location { get; set; }
+        public bool IsFireSafe { get; set; }
+        public int MaxSpectators { get; set; }
+        #endregion
+
+        public Venue(int id, string label, string identifier, string location, bool isFireSafe, int maxSpectators)
+        {
+            Id = id;
+            Label = label;
+            Identifier = identifier;
+            Location = location;
+            IsFireSafe = isFireSafe;
+            MaxSpectators = maxSpectators;
+        }
+
+        public override string ToString()
+        {
+            return "Venue: " + Label + "(" + Identifier + ") " + Location;
+        }
+    }
+}
---
+++ Ufo/DataAccess/IDao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess.DomainClass;
+
+namespace DataAccess
+{
+    interface IDao
+    {
+        int GetSize();
+        IDomain Get(int id);
+        void Store(IDomain domainObj);
+        void Update(IDomain domainObj);
+        void Delete(int id);
+    }
+}
